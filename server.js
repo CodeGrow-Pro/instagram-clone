bb44d90d2edf5b4c166bdb7db46c3fe46664cff2
VishalDevelooperengineer <vk4993102@gmail.com>
2023-03-11 20:32:15 +0530

    add mogodb cloud database

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const mongoose  = require('mongoose');
 const app = require('./index')
 require('dotenv').config()
 mongoose.set('strictQuery', false);
-//mongodb+srv://instagramdb:xthoG0RuqnWqnMtV@cluster0.8v0a0xv.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect(process.env.MONGO_URI+process.env.DB_NAME,{family:4},(err)=>{
+//
+mongoose.connect("mongodb+srv://instagramdb:xthoG0RuqnWqnMtV@cluster0.8v0a0xv.mongodb.net/?retryWrites=true&w=majority",{family:4},(err)=>{
           if(!err){
             console.log('Node envirnment : ', process.env.NODE_ENV)
             console.log("database connected successfully DataBaseName : ", process.env.DB_NAME);
