d3ec3e9cc443dd2d3f19a74feaf2aec4f11f96b1
VishalDevelooperengineer <vk4993102@gmail.com>
2023-03-13 02:56:54 +0530

    issues resolve

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const routers = require('./routes/apis/index')
 const cors = require('cors');
-const fileUpload = require('express-fileupload');
 const app = express();
+app.use(cors())
+const fileUpload = require('express-fileupload');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
-app.use(cors({
-    origin:'*'
-}))
 app.get('/',(req,res)=>{
     return res.status(200).send("welcome to instagram clone app");
 })
 app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : '/uploads/'
+    useTempFiles : true
 }));
 app.use('/instagram',routers);
 
